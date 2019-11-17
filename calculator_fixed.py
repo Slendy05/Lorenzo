880cbb870cddd124e64ca9dcd22868a817e09b46
Slendy05 <57875623+Slendy05@users.noreply.github.com>
2019-11-17 21:33:01 +0100

    Update calculator_fixed.py

--- calculator_fixed.py
+++ calculator_fixed.py
@@ -2,6 +2,4 @@ first_num = input("Please input a number: ")
 second_num = input("Please input a second number: ")
 print(int(first_num) + int(second_num))
 
-# third_num = input("Please input a number: ")
-# fourth_num = input("Please input a second number: ")
 input("Press ENTER to close this window ")
